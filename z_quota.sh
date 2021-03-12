bbc9ea0ea681b01e96521251225fc9568076b3fb
abu0sman <mag@spdag.ru>
2021-03-12 11:01:30 +0300

    Небольшие доработки

--- z_quota.sh
+++ z_quota.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 # Скрипт проверяющий размер каталогов. При привышении размера, каталог становится только для чтения.
-# 8 388 608 - 8 Гиг
 
-
-#quota=8388608;
-quota=3000000;
 message="Квота не привышена";
 
+#Читаем конфигурационный файл
+read -r -a dirz <<< $(more /home/mag/prg/zquota/zquota.conf);
 
 # Парсим вывод комманды du, извлекая из вывода только размер каталога в киллобайтах (переменная ${du_out[0]})
-read -r -a du_out <<< $(du -sk /home/mag/quota);
+#read -r -a du_out <<< $(du -sk /home/mag/quota);
 
-if [ ${du_out[0]} -gt $quota ]
-then 
-	message="Внимание квота привышена!"
-fi
+#if [ ${du_out[0]} -gt $quota ]
+#then 
+#	message="Внимание квота привышена!"
+#fi
 
 echo "Debug output";
-echo "Размер каталога /mnt/quota/mag:" ${du_out[0]};
-echo "Квота составляет:" $quota "КБ";
-echo $message;
+#echo "Размер каталога /mnt/quota/mag:" ${du_out[0]};
+#echo "Квота составляет:" $quota "КБ";
+#echo $message;
+echo "Каталоги:" ${dirz[0]};
